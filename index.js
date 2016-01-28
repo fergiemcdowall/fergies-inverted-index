f50f9793aba2395a497cf09a4fab1a61b614a365
Fergie McDowall <fergusmcdowall@gmail.com>
2016-01-28 22:37:35 +0100

    latest

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log('naturalDB')
+var levelup = require('levelup');
+var r = require("restify");
+var db = levelup("store")
+
+function put(req, res, next) {
+  var body = JSON.parse(req.body)
+  var key = body.key
+  var val = body.value
+  db.put(key, val, function(err) {
+    if (err) {console.log(err)}
+    res.send(JSON.stringify(req.body) + ' inserted');
+    return next();
+  })
+}
+
+function get(req, res, next) {
+  var key = req.params.key
+  db.get(key, function(err, value) {
+    if (err) {console.log(err)}
+    res.send(value);
+    return next();
+  })
+}
+
+var server = r.createServer({
+  name: 'naturalDB',
+  version: require("./package.json").version
+});
+
+server.use(r.bodyParser());
+server.listen(8080);
+server.get('/get/:key', get);
+server.post('/put', put);
+
+console.log('    add: curl -X POST -d \'{"key": "Anette", "value": "girlfriend"}\' localhost:8080/put')
+console.log('    get: curl localhost:8080/get/Anette')
