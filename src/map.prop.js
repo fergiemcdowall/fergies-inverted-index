90d1871fd674483717fdf8bd8a716776b8de9eb5
Fergie McDowall <fergusmcdowall@gmail.com>
2018-11-14 11:49:51 +0100

    make it work on older nodes using babel

--- src/map.prop.js
+++ src/map.prop.js
@@ -3,47 +3,43 @@ module.exports = function (db) {
     var ops = {
       limit: 1,
       gte: key + '!'
-    };
+    }
     return new Promise((resolve, reject) => {
-      db.createKeyStream(ops).on('data', data => {
-        return resolve(data);
-      });
-    });
-  };
+      db.createKeyStream(ops)
+        .on('data', data => { return resolve(data) })
+    })
+  }
 
   const MAX = function (key) {
     var ops = {
       limit: 1,
       lte: key + '￮',
       reverse: true
-    };
+    }
     return new Promise((resolve, reject) => {
-      db.createKeyStream(ops).on('data', data => {
-        return resolve(data);
-      });
-    });
-  };
+      db.createKeyStream(ops)
+        .on('data', data => { return resolve(data) })
+    })
+  }
 
   const DIST = function (key, o) {
-    o = o || {};
+    o = o || {}
     var ops = {
       gte: key + '.' + (o.gte || '') + '!',
       lte: key + '.' + (o.lte || '') + '￮'
-    };
-    console.log(ops);
-    const keys = [];
+    }
+    console.log(ops)
+    const keys = []
     return new Promise((resolve, reject) => {
-      db.createKeyStream(ops).on('data', data => {
-        keys.push(data);
-      }).on('end', () => {
-        return resolve(keys);
-      });
-    });
-  };
+      db.createKeyStream(ops)
+        .on('data', data => { keys.push(data) })
+        .on('end', () => { return resolve(keys) })
+    })
+  }
 
   return {
     DIST: DIST,
     MAX: MAX,
     MIN: MIN
-  };
-};
+  }
+}
