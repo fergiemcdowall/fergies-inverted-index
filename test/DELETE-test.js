6e253d5702ea429a3c8bdeb31e207358586ff700
Fergie McDowall <fergusmcdowall@gmail.com>
2018-12-14 13:50:53 +0100

    adding and deleting now returns whole objects

--- test/DELETE-test.js
+++ test/DELETE-test.js
@@ -34,29 +34,30 @@ test('can add some worldbank data', t => {
   t.plan(1)
   global[indexName].PUT(data).then(result => {
     t.looseEqual(result, [
-      '52b213b38594d8a2be17c780',
-      '52b213b38594d8a2be17c781',
-      '52b213b38594d8a2be17c782',
-      '52b213b38594d8a2be17c783',
-      '52b213b38594d8a2be17c784',
-      '52b213b38594d8a2be17c785',
-      '52b213b38594d8a2be17c786',
-      '52b213b38594d8a2be17c787',
-      '52b213b38594d8a2be17c788',
-      '52b213b38594d8a2be17c789' ])
+      { _id: '52b213b38594d8a2be17c780', board_approval_month: 'November', totalamt: 130000000 },
+      { _id: '52b213b38594d8a2be17c781', board_approval_month: 'November', totalamt: 0 },
+      { _id: '52b213b38594d8a2be17c782', board_approval_month: 'November', totalamt: 6060000 },
+      { _id: '52b213b38594d8a2be17c783', board_approval_month: 'October', totalamt: 0 },
+      { _id: '52b213b38594d8a2be17c784', board_approval_month: 'October', totalamt: 13100000 },
+      { _id: '52b213b38594d8a2be17c785', board_approval_month: 'October', totalamt: 10000000 },
+      { _id: '52b213b38594d8a2be17c786', board_approval_month: 'October', totalamt: 500000000 },
+      { _id: '52b213b38594d8a2be17c787', board_approval_month: 'October', totalamt: 0 },
+      { _id: '52b213b38594d8a2be17c788', board_approval_month: 'October', totalamt: 160000000 },
+      { _id: '52b213b38594d8a2be17c789', board_approval_month: 'October', totalamt: 200000000 } 
+    ])
   })
 })
 
 test('can GET with string', t => {
   t.plan(1)
   global[indexName].GET('board_approval_month:November')
-    .then(result => {
-      t.looseEqual(result, [
-        { _id: '52b213b38594d8a2be17c780', match: [ 'board_approval_month:November' ] },
-        { _id: '52b213b38594d8a2be17c781', match: [ 'board_approval_month:November' ] },
-        { _id: '52b213b38594d8a2be17c782', match: [ 'board_approval_month:November' ] }
-      ])
-    })
+   .then(result => {
+     t.looseEqual(result, [
+       { _id: '52b213b38594d8a2be17c780', match: [ 'board_approval_month:November' ] },
+       { _id: '52b213b38594d8a2be17c781', match: [ 'board_approval_month:November' ] },
+       { _id: '52b213b38594d8a2be17c782', match: [ 'board_approval_month:November' ] }
+     ])
+   })
 })
 
 test('can DELETE', t => {
@@ -65,7 +66,10 @@ test('can DELETE', t => {
     '52b213b38594d8a2be17c781',
     '52b213b38594d8a2be17c782'
   ]).then(result => {
-    t.looseEqual(result, [ '52b213b38594d8a2be17c781', '52b213b38594d8a2be17c782' ])
+    t.looseEqual(result, [
+      { _id: '52b213b38594d8a2be17c781', board_approval_month: 'November', totalamt: 0 },
+      { _id: '52b213b38594d8a2be17c782', board_approval_month: 'November', totalamt: 6060000 }
+    ])
   })
 })
 
