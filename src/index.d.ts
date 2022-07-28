61bfeb590968070b53a88541dfd53d7082734dd6
Mikael Karon <mikael@karon.se>
2022-07-28 17:32:39 +0200

    fix: remove unused code/settings

--- src/index.d.ts
+++
@@ -1,4 +0,0 @@
-import fii = require("./main.js");
-import browser = require("./browser.js");
-import node = require("./node.js");
-export { fii, browser, node };
--- src/index.js
+++
@@ -1,5 +0,0 @@
-const fii = require('./main.js')
-const browser = require('./browser.js')
-const node = require('./node.js')
-
-module.exports = { fii, browser, node }
